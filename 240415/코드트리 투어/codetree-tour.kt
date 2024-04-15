4dfb9dbb4ba238835fe12a2638ea63f69e0626d0
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-04-15 17:22:07 +0900

    Title: 코드트리 투어, Time: 3051ms, Memory: 103MB, Status: Time Limit Exceed - CodeTree

--- 240415/코드트리 투어/codetree-tour.kt
+++ 240415/코드트리 투어/codetree-tour.kt
@@ -145,8 +145,8 @@ fun findBestItem(starting: Int, tourMap: Map<Int, List<Pair<Int, Int>>>, tourIte
                         }
                         break
                     }
-                    
-                    if (price - (nodeWeight[arrivePoint]?: Int.MAX_VALUE) >= (maxEarning?: Int.MAX_VALUE) ) {
+
+                    if (maxEarning == null || (price - (nodeWeight[arrivePoint]?: Int.MAX_VALUE) >= (maxEarning?: Int.MAX_VALUE) )) {
                         val nextNodes = tourMap[arrivePoint] ?: listOf()
                         nextNodes.forEach {
                             priorityQueue.offer(Triple(arrivePoint, it.first, (nodeWeight[arrivePoint]?: Int.MAX_VALUE) + it.second))
